fdec692ac3dc4b57f600574fff4e213cdee9b521
fwcd <fwcdmail@gmail.com>
2023-11-02 17:16:02 +0000

    Add security-scoped accesses

--- Sandbox Explorer/ViewModel/FileSystemViewModel.swift
+++ Sandbox Explorer/ViewModel/FileSystemViewModel.swift
@@ -6,29 +6,22 @@
 //
 
 import Foundation
+import OSLog
+
+private let log = Logger(subsystem: "Sandbox Explorer", category: "FileSystemViewModel")
 
 class FileSystemViewModel: ObservableObject {
     @Published var useSecurityScopedBookmarks = false
     @Published var resolveSymlinks = true
     
-    func lookup(url: URL) -> FileNode {
-        if useSecurityScopedBookmarks {
-            url.startAccessingSecurityScopedResource()
-        }
-        
-        defer {
-            if useSecurityScopedBookmarks {
-                url.stopAccessingSecurityScopedResource()
-            }
-        }
-        
-        let fileManager = FileManager.default
-        
-        if resolveSymlinks, let destination = try? fileManager.destinationOfSymbolicLink(atPath: url.path) {
-            return .symlink(url: url, destination: URL(filePath: destination))
-        }
-            
+    func lookup(url: URL, trySecurityScopedAccess: Bool = true) -> FileNode {
         do {
+            let fileManager = FileManager.default
+            
+            if resolveSymlinks, let destination = try? fileManager.destinationOfSymbolicLink(atPath: url.path) {
+                return .symlink(url: url, destination: URL(filePath: destination))
+            }
+            
             var isDir: ObjCBool = false
             _ = fileManager.fileExists(atPath: url.path, isDirectory: &isDir)
             
@@ -46,6 +39,38 @@ class FileSystemViewModel: ObservableObject {
             
             return .file(url: url, bytes: Int(size))
         } catch {
+            do {
+                securityScope:
+                if trySecurityScopedAccess && useSecurityScopedBookmarks {
+                    let bookmark = try url.bookmarkData(options: .withSecurityScope)
+                    guard !bookmark.isEmpty else {
+                        log.warning("Could not create bookmark")
+                        break securityScope
+                    }
+                    var stale = false
+                    let url = try URL(resolvingBookmarkData: bookmark, bookmarkDataIsStale: &stale)
+                    guard url.startAccessingSecurityScopedResource() else {
+                        log.warning("Could not start accessing security-scoped resource")
+                        break securityScope
+                    }
+                    defer {
+                        url.stopAccessingSecurityScopedResource()
+                    }
+                    var error: NSError? = nil
+                    var fileNode: FileNode? = nil
+                    NSFileCoordinator().coordinate(readingItemAt: url, error: &error) { newUrl in
+                        fileNode = lookup(url: newUrl, trySecurityScopedAccess: false)
+                    }
+                    if let error {
+                        log.warning("Could not coordinate read: \(error)")
+                    }
+                    if let fileNode {
+                        return fileNode
+                    }
+                }
+            } catch {
+                return .inaccessible(url: url, reason: error.localizedDescription)
+            }
             return .inaccessible(url: url, reason: error.localizedDescription)
         }
     }
